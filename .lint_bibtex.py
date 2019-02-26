f4fe1f2f5589afc1a0caf4fdd44ea9fbf63e5361
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-02-26 18:58:41 +0000

    Add .travis.yml to auto formatting

---
+++ .lint_bibtex.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import collections
+import sys
+
+import bibtexparser
+
+
+def main():
+    parser = bibtexparser.bparser.BibTexParser(
+        interpolate_strings=False, homogenize_fields=True
+    )
+    with open('bibtex.bib') as f:
+        try:
+            bib = bibtexparser.load(f, parser=parser)
+        except Exception as e:
+            print('Encounter following error:', file=sys.stderr)
+            print(e)
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
